f672a6c53658d82486f3a1d79c1b110d2dc2f51b
Onkar Jadhav <omjadhav2610@gmail.com>
2024-02-14 23:47:19 +0530

    Cleanup

--- cypress/e2e/l10.cy.js
+++ cypress/e2e/l10.cy.js
@@ -3,17 +3,6 @@
 if (studentSubmissionUrl.endsWith("/")) {
   studentSubmissionUrl = studentSubmissionUrl.slice(0, -1);
 }
-
-function formatDateWithOffset(daysOffset = 0) {
-  const date = new Date(); // Get the current date
-  date.setDate(date.getDate() + daysOffset); // Add or subtract days based on the offset
-
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}-${month}-${day}`;
-}
-
 const firstName = "L10 VTA";
 const lastName = "User";
 const email = "vta@pupilfirst.com";
@@ -26,7 +15,6 @@ describe("Preparing for Level 10 milestone testing, first we will verify signup"
     cy.get('input[name="email"]').should("exist");
     cy.get('input[name="password"]').should("exist");
 
-    // Empty firstName
     cy.get('input[name="firstName"]').type("L10 user");
     cy.get('input[name="email"]').type("vta@pupilfirst.com");
     cy.get('input[name="password"]').type("12345678");
